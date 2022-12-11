893f228a755b4553a85ba170dc9f66427302dd77
Didac Fernandez <didac.fernandez@insiemecloud.net>
2022-12-11 11:05:48 -0800

    implemented router

--- src/App.jsx
+++ src/App.jsx
@@ -1,58 +1,62 @@
-import { useEffect, useState, useRef } from 'react'
+import { useEffect, useState, useRef, Fragment } from 'react'
 import './App.css'
+import { BrowserRouter as Router, Routes, Route  } from 'react-router-dom';
 import Gallery from './Components/Gallery.jsx'
 import SearchBar from './Components/SearchBar.jsx'
 import { DataContext } from './Context/DataContext'
 import { SearchContext } from './Context/SearchContext'
+import AlbumView from './Components/AlbumView';
+import ArtistView from './Components/ArtistView';
+
 
 
 function App ()  {
-    let [search, setSearch] = useState('')
     let [message, setMessage] = useState('Search for Music!')
     let [data, setData] = useState([])
     let searchInput = useRef('')
 
     const API_URL = 'https://itunes.apple.com/search?term='
      
-    useEffect(() => {
-        if (search) {
-          const fetchData = async () => {
-            document.title = `${search} Music`
-            const response = await fetch(API_URL + search)
-            const resData = await response.json()
-            if (resData.results.length > 0) {
-              setData(resData.results)
-            } else {
-              setMessage("Not found.")
-            }
-            console.log(resData)
-          }
-          fetchData()
-        }
-      }, [search])
-
-
     const handleSearch = (e, term) => {
-        e.preventDefault()
-        setSearch(term)
+      e.preventDefault()
+      const fetchData = async () => {
+        document.title = `${term} Music`
+        const response = await fetch(API_URL + term)
+        const resData = await response.json()
+        if (resData.results.length > 0) {
+          return setData(resData.results)
+        } else {
+          return setMessage("Not found.")
+        }
+        console.log(resData)
+      }
+      fetchData()
     }
-
+  
     return (
-        <div className ="App">
-          <SearchContext.Provider value ={{
-        term: searchInput,
-        handleSearch: handleSearch
-          
-          }}>
-            <SearchBar handleSearch={handleSearch} />
-            </SearchContext.Provider>
-            {message}
-            <DataContext.Provider value={data} >
-              <Gallery />
-            </DataContext.Provider>
-        </div>
-    )
-}
-
-export default App;
-
+      <div className="app">
+        {message}
+        <Router>
+          <Routes>
+            <Route path="/" element={
+              <Fragment>
+                <SearchContext.Provider value={{
+                  term: searchInput,
+                  handleSearch: handleSearch
+                }}>
+                  <SearchBar/>
+                </SearchContext.Provider>
+                <DataContext.Provider value={data}>
+                  <Gallery />
+                </DataContext.Provider>
+              </Fragment>
+            }/>
+            <Route path='/album/:id' element={<AlbumView />} />
+            <Route path='/artist/:id' element={<ArtistView/>} />
+         </Routes>
+        </Router>
+      </div>
+    );
+  }
+  
+  export default App;
---
+++ src/Components/ArtistView.jsx
@@ -0,0 +1,16 @@
+// These components will be making separate API calls from the app
+// component to serve specific data about our artist
+import { useState, useEffect } from 'react'
+
+function ArtistView() {
+    const [ artistData, setArtistData ] = useState([])
+
+    return (
+        <div>
+           <h2>The id passed was: {id}</h2> 
+            <p>Artist Data Goes Here!</p>
+        </div>
+    )
+}
+
+export default ArtistView
